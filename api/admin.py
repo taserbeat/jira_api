eee426832fd0afa0fa1d12a7db7ca73998bf1f69
taserbeat <taserbeat@gmail.com>
2020-11-29 17:20:42 +0900

    update admin site

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Category, Task, Profile
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Task)
+admin.site.register(Profile)
